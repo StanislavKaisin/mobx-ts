64288acad91839364a9aed7959c2c42f0225b3d0
StanislavKaisin <41913995+StanislavKaisin@users.noreply.github.com>
2023-03-19 17:53:05 +0200

    added endpoint, api models

---
+++ src/api/models/models.ts
@@ -0,0 +1,73 @@
+export interface IAdress {
+  address: string;
+  city: string;
+  coordinates: {
+    lat: number;
+    lng: number;
+  };
+  postalCode: string;
+  state: string;
+}
+export interface IUser {
+  id: number;
+  firstName: string;
+  lastName: string;
+  maidenNam: string;
+  age: number;
+  gender: string;
+  email: string;
+  phone: string;
+  username: string;
+  password: string;
+  birthDate: string;
+  image: string;
+  bloodGroup: string;
+  height: number;
+  weight: number;
+  eyeColor: string;
+  hair: {
+    color: string;
+    type: string;
+  };
+  domain: string;
+  ip: string;
+  address: IAdress;
+  macAddress: string;
+  university: string;
+  bank: {
+    cardExpire: string;
+    cardNumber: string;
+    cardType: string;
+    currency: string;
+    iban: string;
+  };
+  company: {
+    address: IAdress;
+    department: string;
+    name: string;
+    title: string;
+  };
+  ein: string;
+  ssn: string;
+  userAgent: string;
+}
+
+export interface IProduct {
+  id: number;
+  title: string;
+  price: number;
+  quantity: number;
+  total: number;
+  discountPercentage: number;
+  discountedPrice: number;
+}
+
+export interface ICart {
+  id: number;
+  products: IProduct[];
+  total: number;
+  discountedTotal: number;
+  userId: number;
+  totalProducts: number;
+  totalQuantity: number;
+}
